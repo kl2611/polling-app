6fa361d54b1714e8094311fae3b3849bc92a5da4
Kelly <kelly.liu@columbia.edu>
2017-10-17 17:59:42 -0700

    first commit

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from models import db
+
+app = Flask(__name__)
+
+# load config from the config file we created earlier
+app.config.from_object('config')
+
+# initialize and create the database
+db.init_app(app)
+db.create_all(app=app)
+
+@app.route('/')
+def home():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run()
