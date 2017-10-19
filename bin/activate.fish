f1789b0d5f0042bcda8ec45df146931c4aa5aa78
Kelly <kelly.liu@columbia.edu>
2017-10-19 00:52:47 -0700

    updated bin

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Kelly/Desktop/polling-app"
+set -gx VIRTUAL_ENV "/Users/Kelly/Desktop/test/polling-app"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
